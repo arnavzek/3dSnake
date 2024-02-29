e59a83f1e9a8ce730c868ec0176632f2d506587d
Arnav Singh <arnav010singh@gmail.com>
2024-02-29 22:33:31 +0530

    First commit

---
+++ Assets/SnakeMovement.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SnakeMovement : MonoBehaviour
+{
+
+    public GameObject bodyPartPrefab;
+
+    private List<GameObject> bodyParts = new List<GameObject>();
+    private List<Vector3> posHistory = new List<Vector3>();
+
+    public float speed;
+    public float steerSpeed;
+    public int gap = 10;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        transform.position += transform.forward * speed * Time.deltaTime;
+
+        transform.Rotate(transform.up * Input.GetAxis("Horizontal") * steerSpeed * Time.deltaTime);
+
+        posHistory.Insert(0, transform.position);
+
+        int index = 1;
+        foreach(var part in bodyParts)
+        {
+            Vector3 newPos = posHistory[Mathf.Min(index * gap, posHistory.Count -1)];
+            part.transform.position = newPos;
+            index++;
+        }
+
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Food")
+        {
+            Grow();
+            Destroy(collision.gameObject);
+        }
+    }
+
+    private void Grow()
+    {
+        GameObject newPart = Instantiate(bodyPartPrefab);
+        bodyParts.Add(newPart);
+    }
+}
